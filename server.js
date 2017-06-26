1353e5422aa911e8ce80797d2b4958fff17bca37
Diego Armando Amarillas Lara <diego.amarillas@ctinter.com.mx>
2017-06-26 12:25:20 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', hbs);
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    next();
+});
+
+/*
+app.use((req,res,next) => {
+    res.render('down.hbs');
+});
+*/
+
+app.use(express.static(__dirname  + '/public'));
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase())
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        title: 'Home page',
+        msg: 'Welcome to some site'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        title: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'an error'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server us up');
+});
